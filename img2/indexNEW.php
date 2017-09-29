3fc2886d8e3310d1d02cd2aec42518fdb92828e0
oxfordshiredigital <james@oxfordshiredigital.com>
2017-09-29 10:36:56 +0100

    New site uploaded

---
+++ img2/indexNEW.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Boshers » Master Builders</title>
+        <!-- Bootstrap -->
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- Font -->
+        <link href="https://fonts.googleapis.com/css?family=Lato:300,400" rel="stylesheet">
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+        <style>
+            p, a, a:hover {font-family: 'Lato', sans-serif; color: black; text-decoration: none; }
+            p {font-size: 24px; font-weight: 300;}
+            a {cursor: pointer;}
+            .contact-details {text-align: right;}
+            @media (max-width: 1000px) {
+            .contact-details {text-align: center;}
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <br class="hidden-xs hidden-sm">
+            <br class="hidden-xs hidden-sm">
+            <br>
+            <div class="row">
+                <div class="col-md-4 col-sm-12">               
+                    <img src="773d91890392416b8bc9bcf56611b367.png" class="img-responsive center-block">
+                </div>
+                <div class="col-md-2"></div>
+                <div class="col-md-6 col-sm-12 center-block contact-details">
+                    <br class="hidden-xs hidden-sm">
+                    <br class="hidden-xs hidden-sm">
+                    <a href="tel:01582 344 866">01582 344 866</a>
+                    <br>
+                    <a href="mailto:email@boshersltd.com">email@boshersltd.com</a>
+                <br>
+                    <a>Charlotte's Court, Prudence Place, Proctor Way, Luton, LU2 9PE</a>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <img alt="splash-image" class="img-responsive" src="splash-image.jpg"/>
+            </div>
+            <br>
+            <div class="row" style="text-align: center;">
+                <p style="padding: 30px;">Our website is currently being updated - see you soon.</p>
+            </div>
+
+
+
+
+        </div><!-- /.container -->
+
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+    </body>
+</html>
